c39ae0b8a287580400e9365e1d6a046ca22bab03
Tim Kwak <tim.k.kwak@gmail.com>
2018-02-21 16:27:17 -0500

    Change document order

--- parsejson.js
+++ parsejson.js
@@ -1,17 +1,11 @@
 const myJson = require("./iba_official_cocktails.json");
 const fs = require('fs');
 
-let data = Object.keys(myJson);
+let file = fs.createWriteStream('input.json');
 
-data = data.map(key => {
-  let temp = myJson[key];
-  temp["name"] = key;
-  return temp;
-});
-
-let file = fs.createWriteStream('input.txt');
-
-data.forEach( line => {
+Object.keys(myJson).map(key => Object.assign({
+  "name": key
+}, myJson[key])).forEach(line => {
   file.write(JSON.stringify(line) + '\n');
 });
 
